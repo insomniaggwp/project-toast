844fe23c1297e252b25217916e2998b7cbf73ac0
arif hidayat <ariefhide94@gmail.com>
2025-01-17 12:47:02 +0700

    task 6

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,9 +1,16 @@
 import React from "react";
+import useEscapeKey from '../../hooks/useEscapeKey';
 
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState(() => []);
+
+  const handleEscape = React.useCallback(() => {
+    setToasts([]);
+  }, []);
+  
+  useEscapeKey(handleEscape);
 
   const addToast = React.useCallback((message, variant) => {
   const nextToasts = [
